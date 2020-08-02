c67e4a90b233ab4860c975b622cc576be593cc65
SangkyuGoodboyYesDoitsgoi <wjdruf23123@gmail.com>
2020-08-02 14:55:15 +0900

    map association search api link

--- routes/Relevant.js
+++ routes/Relevant.js
@@ -1,25 +1,40 @@
 var express = require('express');
+const { response } = require('express');
 var router = express.Router();
+var asycArr = new Array();
 
+//연관검색어를 생성
 router.get('/map', (req, res) => {
-    let keyword = req.query.keyword;
-
+    const keyword = req.query.keyword;
+    console.log(keyword)
+    const URL = "https://m.map.naver.com/ac.map/mobilePlaceAddress/ac?q=" + encodeURI(keyword) + "&st=10&r_lt=10";
     var request = require('request');
-    // appkey=0f91ccc64a97c44c9f5e102f155b54d4&libraries=services,clusterer,drawing
-    var url = {
-       url : "https://map.kakao.com/api/dapi/suggest/place?q=부산",
-       header : {appkey:"0f91ccc64a97c44c9f5e102f155b54d4",libraries:"services,clusterer,drawing"}
-    };
-    request.get("https://map.kakao.com/api/dapi/suggest/place?_caller1=ver_map_141&q=%EC%84%9C%EC%9A%B8%EC%8B%9C", (error, response, body) => {
-        console.log(response);
-        console.log(body);
+    let keyword_list = new Array();
+    request.get(URL, (error, response) => {
         if(error){
-            console.log(error);
+            res.send(keyword_list);
         }
-
-        res.send("asd");
+        // var test1 = JSON.parse(response.body).items[0];
+        // console.log(test1[0]);
+        res.send(JSON.parse(response.body).items[0])
     })
-
-})
+    
+    
+    // const url = "https://map.kakao.com/api/dapi/suggest/place?_caller1=ver_map_141&q=" + encodeURI(keyword);
+    //
+    // request.get(url, (error, response) => {
+    //     if(error){
+    //         res.send(keyword_list);
+    //     }
+    //     const items = JSON.parse(response.body).items;
+    //     if(items.length > 0){
+    //         let j = items.length >= 10 ? 10 : items.length;
+    //         for(let i = 0; i < j; i++){
+    //             keyword_list.push(items[i].split('|')[0]);
+    //         }
+    //     }
+    //     res.send(keyword_list);
+    // })
+});
 
 module.exports = router;
